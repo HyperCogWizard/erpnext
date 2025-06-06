224859ded44feb9dfa5e97b48d7844701e20759e
Faris Ansari <netchamp.faris@gmail.com>
2025-06-06 20:05:34 +0530

    fix: update sql function usage syntax

--- erpnext/stock/doctype/purchase_receipt/test_purchase_receipt.py
+++ erpnext/stock/doctype/purchase_receipt/test_purchase_receipt.py
@@ -1808,7 +1808,7 @@ class TestPurchaseReceipt(IntegrationTestCase):
 				"voucher_no": pr.name,
 				"is_cancelled": 0,
 			},
-			fieldname=["sum(stock_value_difference)"],
+			fieldname=[{"SUM": "stock_value_difference"}],
 		)
 
 		# Value of Stock Account should be equal to the sum of Stock Value Difference
